826001035624b310c7f20f751423927ee66186a4
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-14 16:39:55 -0800

    detect and read gyroscope

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import sayHello from './hello';
 import './index.scss';
+import GyroNorm from 'gyronorm';
 
-document.getElementById('root').innerHTML = sayHello();
+let gn = new GyroNorm();
 
+console.log('gn', gn);
 
+gn.init().then(function(){
+  gn.start(function(data){
+    console.log('data', data);
+    // Process:
+    // data.do.alpha	( deviceorientation event alpha value )
+    // data.do.beta		( deviceorientation event beta value )
+    // data.do.gamma	( deviceorientation event gamma value )
+    // data.do.absolute	( deviceorientation event absolute value )
+
+    // data.dm.x		( devicemotion event acceleration x value )
+    // data.dm.y		( devicemotion event acceleration y value )
+    // data.dm.z		( devicemotion event acceleration z value )
+
+    // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
+    // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
+    // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
+
+    // data.dm.alpha	( devicemotion event rotationRate alpha value )
+    // data.dm.beta		( devicemotion event rotationRate beta value )
+    // data.dm.gamma	( devicemotion event rotationRate gamma value )
+  });
+}).catch(function(e){
+  // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
+});
