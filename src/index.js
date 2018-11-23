402bf03af68dd27b90ddcc77b8f8722ad6c14ef3
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-22 20:55:20 -0800

    lets do our first ass shaking test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
+import Alpha from './mp3/alpha.mp3'
+import Beta from './mp3/beta.mp3'
+import Gamma from './mp3/gamma.mp3'
+
 import './index.scss';
+
 const orientation = document.getElementById('orientation');
 const rotationrate = document.getElementById('rotationrate');
 const acceleration = document.getElementById('acceleration');
 const accelerationgravity = document.getElementById('accelerationgravity');
+
+let ap = {
+    alpha: new Audio(Alpha),
+    beta: new Audio(Beta),
+    gamma: new Audio(Gamma)
+};
+/*
 let o = 0;
 window.addEventListener('deviceorientation', e => {
     if (o % 5 === 0) orientation.innerText = `Orientation: \n\n Alpha: ${e.alpha} \n Beta: ${e.beta} \n\Gamma: ${e.gamma} \n\n\n`;
     o++;
 }, true);
-
+*/
 let rr = 0;
 let a = 0;
 let ag = 0;
@@ -21,4 +33,9 @@ window.addEventListener('devicemotion', e => {
 
     if (ag % 5 === 0) accelerationgravity.innerText = `Acceleration w gravity \n\n x: ${e.accelerationIncludingGravity.x} \n y: ${e.accelerationIncludingGravity.y} \n z: ${e.accelerationIncludingGravity.z} \n\n\n`;
     ag++;
+
+    if (e.rotationRate.alpha > 100) ap.alpha.play();
+    if (e.rotationRate.beta > 100) ap.beta.play();
+    if (e.rotationRate.gamma > 100) ap.gamma.play();
+
 }, true);
