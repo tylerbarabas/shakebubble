a98284719a942a063d631016d29cc87c8bfb316d
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-22 19:47:55 -0800

    slow down refresh rate, make easy to read

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './index.scss';
 const root = document.getElementById('root');
+let count = 0;
 window.addEventListener('deviceorientation', e => {
-    root.innerText = `Alpha: ${e.alpha} Beta: ${e.beta} Gamma: ${e.gamma} `;
+    if (count % 5 === 0) root.innerText = `Alpha: ${e.alpha} \n\n Beta: ${e.beta} \n\n Gamma: ${e.gamma} `;
+    count++;
 }, true);
