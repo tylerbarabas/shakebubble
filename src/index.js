f1fe55e16c75552c325cd31dffbfeaf7c78b9ad7
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-22 20:07:26 -0800

    device motion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import './index.scss';
-const root = document.getElementById('root');
+const orientation = document.getElementById('orientation');
+const motion = document.getElementById('motion');
 let count = 0;
 window.addEventListener('deviceorientation', e => {
     if (count % 5 === 0) root.innerText = `Alpha: ${e.alpha} \n\n Beta: ${e.beta} \n\n Gamma: ${e.gamma} `;
     count++;
+}, true);
+
+window.addEventListener('devicemotion', e => {
+    console.log('e', e);
+    motion.innerText = e;
 }, true);
