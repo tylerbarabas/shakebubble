122cf69429d1bcfda527e9f06ad7d5d1c14de459
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-22 20:28:00 -0800

    fix the variable

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ let rr = 0;
 let a = 0;
 let ag = 0;
 window.addEventListener('devicemotion', e => {
-    if (rr % 5 === 0) rotationrate.innerText = `Rotation Rate: \n\n x: ${e.rotationRate.x} \n y: ${e.rotationRate.y} \n z: ${e.rotationRate.z} \n\n\n`;
+    if (rr % 5 === 0) rotationrate.innerText = `Rotation Rate: \n\n x: ${e.rotationRate.alpha} \n y: ${e.rotationRate.beta} \n z: ${e.rotationRate.gamma} \n\n\n`;
     rr++;
 
     if (a % 5 === 0) acceleration.innerText = `Acceleration \n\n x: ${e.acceleration.x} \n y: ${e.acceleration.y} \n z: ${e.acceleration.z} \n\n\n`;
