fab04d9b5d3324f1b15601003deabe854e3bb464
Tyler Barabas <tylerbarabas@gmail.com>
2018-11-23 00:00:25 -0800

    higher retrigger thresh

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ let RRG = 0;
 let highestRRG = 0;
 
 let shouldTrigger = true;
-let triggerThresh = 100;
+let triggerThresh = 500;
 const triggerSafety = () => {
     shouldTrigger = false;
     setTimeout(() => {
